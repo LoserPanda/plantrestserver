03fc80181ff82daeddce7b86993e92239fd21f31
keke5000 <keke5000gmail.com>
2018-08-30 14:17:02 +0300

    hehe

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
+const mysql = require('mysql')
+const bodyParser = require('body-parser');
+const con = require('../config/serverconfig');
 
-//CONNECT TO THE DATABASE
-const mysql = require('mysql');
-const dbconfig = require('../config/dbconfig');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
-const connection = mysql.createConnection(dbconfig.dbconnection);
-
-connection.connect(err => {
-    if (err) throw err;
-    console.log('Connected to the database');
-
-    const authCheck = (req, res, next) => {
-        if (!req.user) {
-            res.redirect('/auth/login');
-        } else {
-            console.log(req.user, "jou");
-            next();
-        }
-    };
-
-    //GET ALL USERS
-    router.get('/', authCheck, (req, res, next) => {
-        connection.query('SELECT * FROM users', (err, results) => {
-            if (err) throw err;
-            console.log(results);
-            res.send(results);
-        });
-    });
-
-    //GET USER BY ID
-    router.get('/:userID', authCheck, (req, res, next) => {
-        connection.query('SELECT * FROM users WHERE userID = ?', [req.params.userID], (err, results) => {
-            if (err) throw err;
-            //console.log(results);
-            res.send(results);
-        });
-    });
-
-    //POST NEW USER TO THE DATABASE
-    router.post('/', authCheck, (req, res, next) => {
-        connection.query('INSERT INTO users (username, email) VALUES (?, ?)', [req.body.username, req.body.email], (err, results) => {
-            if (err) throw err;
-            console.log(results);
-            res.send(results);
-        });
-    });
-
+const connection = mysql.createConnection({
+    host: con.connection.host,
+    user: con.connection.user,
+    password: con.connection.password,
+    database: con.connection.database
 });
 
+connection.connect(function (err) {
+    if (err) throw err
+    console.log('You are now connected...')
+
+    router.get('/', function (req, res) {
+        connection.query('SELECT * FROM sensordata', function (err, results) {
+            if (err) throw err;
+            res.json(results);
+        });
+    });
+
+
+})
 module.exports = router;
