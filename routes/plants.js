223f06019ab1bad0ecf78aa94fe1d87b521b5395
keke5000 <keke5000gmail.com>
2018-09-03 18:42:31 +0300

    plant crud add

--- routes/plants.js
+++ routes/plants.js
@@ -11,19 +11,27 @@ connection.connect(err => {
     if (err) throw err;
     console.log('Connected to the database');
 
-    // //GET ALL USERS
-    // router.get('/', (req, res, next) => {
-    //     console.log("uuseri lakaa", req.user, "palautettu uuuseri lol");
-    //     connection.query('SELECT * FROM users',(err, results) => {
+    //GET PLANTS BY USER ID
+    router.get('/:userID', (req, res, next) => {
+        connection.query('SELECT * FROM plants WHERE userID = ?', [req.params.userID], (err, results) => {
+            if (err) throw err;
+            //console.log(results);
+            res.send(results);
+        });
+    });
+
+    // //GET SENSORDATA AND SENSORS
+    // router.get('/byuserid/:userID', (req, res, next) => {
+    //     connection.query('SELECT sensordata.time, sensordata.light, sensordata.humidity, sensordata.temperature, sensordata.soilmoisture FROM sensordata INNER JOIN sensors ON sensors.sensorID=sensordata.sensorID WHERE userID = ?', [req.params.userID], (err, results) => {
     //         if (err) throw err;
-    //         console.log(results);
+    //         //console.log(results);
     //         res.send(results);
     //     });
     // });
 
-    //GET PLANTS BY USER ID
-    router.get('/:userID', (req, res, next) => {
-        connection.query('SELECT * FROM plants WHERE userID = ?', [req.params.userID], (err, results) => {
+    //GET PLANTS SENSOR SOIL (SOIL AVG TEST)
+    router.get('/averages/:userID', (req, res, next) => {
+        connection.query('SELECT soilAvg, lightAvg, temperatureAvg, humidityAvg FROM plants INNER JOIN users ON users.plantID=plants.plantID WHERE userID = ?', [req.params.userID], (err, results) => {
             if (err) throw err;
             //console.log(results);
             res.send(results);
