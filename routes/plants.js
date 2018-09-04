3ca1a4332a7679b4fde981e0a6e3a4652ca814b8
LoserPanda <loserpanda82@gmail.com>
2018-09-04 12:53:03 +0300

    megaforcepush

--- routes/plants.js
+++ routes/plants.js
@@ -11,14 +11,14 @@ connection.connect(err => {
     if (err) throw err;
     console.log('Connected to the database');
 
-    //GET PLANTS BY USER ID
-    router.get('/:userID', (req, res, next) => {
-        connection.query('SELECT * FROM plants WHERE userID = ?', [req.params.userID], (err, results) => {
-            if (err) throw err;
-            //console.log(results);
-            res.send(results);
-        });
-    });
+    // //GET PLANTS BY USER ID
+    // router.get('/:userID', (req, res, next) => {
+    //     connection.query('SELECT * FROM plants WHERE userID = ?', [req.params.userID], (err, results) => {
+    //         if (err) throw err;
+    //         //console.log(results);
+    //         res.send(results);
+    //     });
+    // });
 
     // //GET SENSORDATA AND SENSORS
     // router.get('/byuserid/:userID', (req, res, next) => {
@@ -29,23 +29,23 @@ connection.connect(err => {
     //     });
     // });
 
-    //GET PLANTS SENSOR SOIL (SOIL AVG TEST)
-    router.get('/averages/:userID', (req, res, next) => {
-        connection.query('SELECT soilAvg, lightAvg, temperatureAvg, humidityAvg FROM plants INNER JOIN users ON users.plantID=plants.plantID WHERE userID = ?', [req.params.userID], (err, results) => {
-            if (err) throw err;
-            //console.log(results);
-            res.send(results);
-        });
-    });
+    // //GET PLANTS SENSOR SOIL (SOIL AVG TEST)
+    // router.get('/averages/:userID', (req, res, next) => {
+    //     connection.query('SELECT soilAvg, lightAvg, temperatureAvg, humidityAvg FROM plants INNER JOIN users ON users.plantID=plants.plantID WHERE userID = ?', [req.params.userID], (err, results) => {
+    //         if (err) throw err;
+    //         //console.log(results);
+    //         res.send(results);
+    //     });
+    // });
 
-    //POST NEW PLANT TO THE DATABASE
-    router.post('/', (req, res, next) => {
-        connection.query('INSERT INTO plants (plantID, name, photolink, userID) VALUES (?, ?, ?, ?)', [req.body.plantID, req.body.name, req.body.photolink, req.body.userID], (err, results) => {
-            if (err) throw err;
-            console.log(results);
-            res.send(results);
-        });
-    });
+    // //POST NEW PLANT TO THE DATABASE
+    // router.post('/:userID', (req, res, next) => {
+    //     connection.query('INSERT INTO plants (name, photolink, soilAvg, lightAvg, humidityAvg, temperatureAvg) VALUES (?, ?, ?, ?, ?, ?) INNER JOIN users ON users.plantID=plants.plantID WHERE userID = ?', [req.body.name, req.body.photolink, req.body.soilAvg, req.body.lightAvg, req.body.humidityAvg, req.body.temperatureAvg. req.params.userID], (err, results) => {
+    //         if (err) throw err;
+    //         console.log(results);
+    //         res.send(results);
+    //     });
+    // });
 
 });
 
